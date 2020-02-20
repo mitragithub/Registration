faf01a2076afdf8c2fad4d43d26c063c747b6942
dtward <dtward@cis.jhu.edu>
2020-02-20 11:41:12 -0500

    removed comments

--- find_centers_for_initialization_nissl.m
+++ find_centers_for_initialization_nissl.m
@@ -1,25 +1,13 @@
-% find centers simple
+% find centers
 % initial slice alignment based on finding centers
 function find_centers_for_initialization_nissl(target_dir, pattern, output_dir, down)
-% clear all;
-% close all;
-% fclose all;
-% addpath /cis/home/dtward/Functions/plotting
-% addpath /cis/home/dtward/Functions/downsample
-% local
-% keyboard
 addpath Functions/plotting
 addpath Functions/downsample
 if nargin < 4
     down = 3*4;
 end
 
-
-
 %%
-% target_dir = '/cis/home/dtward/Documents/intensity_transform_and_missing_data/csh_slices/toDaniel/MD710/';
-% target_dir = '/cis/home/dtward/Documents/intensity_transform_and_missing_data/csh_slices/toDaniel/MD705/';
-% target_dir = '/cis/home/dtward/Documents/intensity_transform_and_missing_data/csh_slices/toDaniel/MD711/';
 geometry_file = dir([target_dir '*.csv']);
 fid = fopen([target_dir geometry_file(1).name],'rt');
 line = fgetl(fid); % ignore the first line
@@ -41,7 +29,8 @@ while 1
     count = count + 1;
     % process this line, splitting at commas
     csv_data(count,:) = strsplit(line,',');
-    %     
+    
+    
 end
 fclose(fid);
 files = csv_data(:,1);
@@ -57,10 +46,10 @@ isthicks = cellfun(@(x) str2num(x), csv_data(:,7)) > 20;
 AJ = zeros(3,3,length(files));
 % load and downsample
 for i = 1 : length(files)
-%     if i == 14;keyboard;end
+
     disp(['downsampling ' num2str(i) ' of ' num2str(length(files))])
     I_ = imread([target_dir files{i}]);
-%     W_ = double(~(I_(:,:,1) == 255 & I_(:,:,2) == 255 & I_(:,:,3) == 255));
+
     W_ = 1.0 - detect_padding_in_nissl(I_);
     I_ = double(I_)/255.0;
     [~,~,Wd_] = downsample2D(1:size(I_,2),1:size(I_,1),W_,down*[1,1]);
@@ -68,17 +57,13 @@ for i = 1 : length(files)
     for c = 1 : 3
         [~,~,Id_(:,:,c)] = downsample2D(1:size(I_,2),1:size(I_,1),I_(:,:,c),down*[1,1]);
     end
-    
-%     Isave{i} = Id_;
-%     Wsave{i} = Wd_;
 
-%%
-% find centers
-%     I = Isave{i};
-%     W = Wsave{i};
+    
+    %%
+    % find centers
     I = Id_;
     W = Wd_;
-    % hopefully avoid artifacts near edges
+    % avoid artifacts near edges
     buf = 5;
     if buf > size(I,1) || buf > size(I,2)
         buf = min([size(I,1),size(I,2)]);
@@ -99,14 +84,8 @@ for i = 1 : length(files)
     axis image
     
     % we want to find the center of mass in some sense
-    for c = 1 : 3
-        %         themax(c) = max(max(I(:,:,c).*(W==1)));
-%         tmp = I(:,:,c).*(W==1);
-        
-        % in some examples there are a lot of pixels that are exactly equal
-        % to 1, the above leads to nans      
+    for c = 1 : 3 % color
         tmp = I(:,:,c).*(W>0.5);
-%         themax(c) = max(tmp(:));
         themax(c) = quantile(tmp(:),0.95);
     end
     test = mean(bsxfun(@minus, reshape(themax,[1,1,3]),I),3);
@@ -136,12 +115,7 @@ for i = 1 : length(files)
     
 end
 %%
-% save(['721_initializer_A.mat'],'AJ');
-% save([target_dir 'initializer_A.mat'],'AJ');
 if ~exist(output_dir,'dir')
     mkdir(output_dir);
 end
 save([output_dir 'initializer_A.mat'],'AJ');
-
-% there is a problem where these guys are not zero mean
-% this will be dealt with in the mapping script
